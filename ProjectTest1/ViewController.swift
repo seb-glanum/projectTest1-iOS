3a3cf7e0a644ed1d9f934555009b1bf7ad2e5bdc
iMac <iMac@iMac-Openspace.local>
2021-06-03 18:23:33 +0200

    first tableview with name of the images files

---
+++ ProjectTest1/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ProjectTest1
+//
+//  Created by iMac on 03/06/2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                print(item, "Okay this a thing !!!")
+                pictures.append(item)
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row].lowercased()
+        return cell
+    }
+}
+
